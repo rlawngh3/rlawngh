1f2524304a12e58a055da64462783385d7d97f76
rlawngh3 <jkl9402123@gmail.com>
2025-11-16 03:28:48 +0900

    Add files via upload

---
+++ python workwpace/homework.py
@@ -0,0 +1,9 @@
+N = int(input())        
+numbers = input()      
+
+total = 0               
+
+for ch in numbers:     
+    total += int(ch)    
+
+print(total)           
---
+++ python workwpace/homework2.py
@@ -0,0 +1,7 @@
+N = int(input())              
+numbers = list(map(int, input().split()))   
+
+minimum = min(numbers)       
+maximum = max(numbers)       
+
+print(minimum, maximum)       
